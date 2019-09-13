c4f09e5ae1188f2b9e7b83f75fd7659a6bb8ef36
Tamarind J <dagwisde@gmail.com>
2019-09-12 23:05:07 -0400

    Set variables for list items

--- js/tasks.js
+++ js/tasks.js
@@ -1,5 +1,19 @@
 // Select elements
 const dateElement = document.querySelector('#date');
+const clear = document.querySelector('.clear');
+const taskList = document.querySelector('#list');
+const input = document.querySelector('#input');
+
+// Storage variables
+let list, id;
+
+// Class toggle variables
+const check = 'fas';
+const uncheck = 'far';
+const line_through = 'lineThrough';
+
+// Function to add task
+
 
 // Show today's date in header
 const dateOptions = {
